5c2f226d9607e5cd6db0b6f62ae9c9ce7521f5cc
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2021-09-15 22:31:03 +0530

    Create first and last digit.py

---
+++ first and last digit.py
@@ -0,0 +1,12 @@
+# cook your dish here
+try:
+    t = int(input())
+    while(t!=0):
+        n = input()
+        f = n[0]
+        l = n[-1]
+        print(int(f)+int(l))
+        t -=1
+except:
+    pass
+    
