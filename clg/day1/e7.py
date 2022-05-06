68ddf88bb7b32a79482399ff33c1f2ba84bbf161
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-05-07 00:27:55 +0530

    Add files via upload

---
+++ clg/day1/e7.py
@@ -0,0 +1,12 @@
+nl = [1,2,3,4]
+sl = ['a','b','c']
+result = zip()
+print(list(result))
+result_list = list(result)
+print(result_list)
+
+result = zip(nl,sl)
+rl = list(result)
+print(rl)
+rs = set(result)
+print(rs)
