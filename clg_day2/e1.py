5b59935c0ea82b42ad56221eb79799df4afd12ec
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-05-10 22:10:11 +0530

    Add files via upload

---
+++ clg_day2/e1.py
@@ -0,0 +1,6 @@
+def print_name(name,stream,college):
+    print(name + " & " + stream + " & " + college)
+
+#n = input("Enter your name: ")   
+#s = input("Enter your stream: ") 
+print_name("Tanuka",stream="ECE",college="NSEC")
