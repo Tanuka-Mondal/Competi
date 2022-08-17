22cd5864ee9c42ed3cb3d391b7e029b815e36afc
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-08-17 14:22:28 +0530

    Create code.py

---
+++ hackerrank/Sales by Match/code.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    return sum([ar.count(i)//2 for i in set(ar)])      
+         
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
